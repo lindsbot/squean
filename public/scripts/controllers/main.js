2eecffa668aa8ccb9a3043d1f9fbce06175b9ec3
Lindsay Hertz <lindsay.hertz@gmail.com>
2013-09-13 11:58:16 -0700

    set up view routes

--- public/scripts/controllers/main.js
+++ public/scripts/controllers/main.js
@@ -1,26 +1,26 @@
 'use strict';
 
-angular.module('phantomRunnerApp');
-angular.module('link', [])
-  .directive('activeLink', [$location, function (location){
-    return {
-      restrict: 'A',
-      link: function (scope, element, attrs, controller){
-        var myClass = attrs.activeLink;
-        var path = attrs.href;
-        path = path.substring(1);
-        scope.location = location;
+// angular.module('phantomRunnerApp');
+// angular.module('link', [])
+//   .directive('activeLink', [$location, function (location){
+//     return {
+//       restrict: 'A',
+//       link: function (scope, element, attrs, controller){
+//         var myClass = attrs.activeLink;
+//         var path = attrs.href;
+//         path = path.substring(1);
+//         scope.$location = location;
 
-        scope.$watch('location.path()', function (newPath){
-          if (path === newPath) {
-            element.addClass(myClass);
-          } else {
-            element.removeClass(myClass);
-          }
-        });
-      }
-    };
-  }]);
+//         scope.$watch('location.path()', function (newPath){
+//           if (path === newPath) {
+//             element.addClass(myClass);
+//           } else {
+//             element.removeClass(myClass);
+//           }
+//         });
+//       }
+//     };
+//   }]);
 
   //   .controller('MainCtrl', '$scope', function ($scope){
   //   $scope.awesomeThings = [
