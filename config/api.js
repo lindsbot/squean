dc8215ec7ec9eba5f360960b94444a1ea8e4dec9
Hack Reactor Students <github@hackreactor.com>
2013-09-13 15:06:11 -0700

    Moved api from Api. js back to routes, testing calls to RunKeeper dummy

--- config/api.js
+++ config/api.js
@@ -1,10 +1,7 @@
-var http = require('http');
+'use strict';
+
 var request = require('request');
 
-module.exports = {
-  request('http://www.google.com', function (error, response, body) {
-    if (!error && response.statusCode === 200) {
-      console.log(body) // Print the google web page.
-    }
-  })
+exports.getRunKeeper = function(){
+
 };
--- config/routes.js
+++ config/routes.js
@@ -2,6 +2,7 @@
 
 var Users = require('../config/db.js').Users;
 var Races = require('../config/db.js').Races;
+var request = require('request');
 var raceUsers = require('../config/db.js').Race_Users;
 var apis = require('../config/api.js');
 
@@ -15,32 +16,39 @@ module.exports = function(app){
 
   app.get('/races', function(req, res, next){
     Races.findAll().complete(function(err, results){
-      if(err) return next(err);
+      if(err) { return next(err); }
       res.json(results);
     });
   });
 
   app.get('/users', function(req, res, next){
     Users.findAll().complete(function(err, results){
-      if(err) return next(err);
+      if(err) { return next(err); }
       res.json(results);
     });
   });
 
   app.get('/raceUsers', function(req, res, next){
     raceUsers.findAll().complete(function(err, results){
-      if(err) return next(err);
+      if(err) { return next(err); }
       res.json(results);
+    });
+  });
+
+  app.get('/test-api', function(req, res, next){
+    res.json({
+      'foo' : 'bar'
     });
   });
 
   app.get('/runKeeper', function(req, res, next){
-    raceUsers.findAll().complete(function(err, results){
-      if(err) return next(err);
-      res.json(results);
+    request.get({
+      url: 'http://www.reddit.com/subreddits/popular.json',
+      json: true
+    }, function(err, _res, body){
+      if(err) { return next(err); }
+      console.log(res);
+      res.json(body);
     });
   });
-
-
-
 };
