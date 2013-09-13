35c5fd3cbc2b4aeddbfbeff45948269235edcde5
Hack Reactor Students <github@hackreactor.com>
2013-09-13 16:19:47 -0700

    Added oauth-config file.

---
+++ config/oauth-providers.js
@@ -0,0 +1,8 @@
+var OAught = require('oauth').OAuth;
+var oauth = new OAuth 
+module.exports = {
+  healthGraph: {
+    appId: '8fd4c695915d48dabd04fc5f6fde04b3',
+    appSecret: '3d7c7adf0dcd46d5b953dcd738a4ab1f'
+  }
+};
