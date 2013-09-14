1bcb50021014d0a13f0c7f13691b84f0c61675fd
Hack Reactor Students <github@hackreactor.com>
2013-09-14 16:19:59 -0700

    add views for How It Works and Contact

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -11,11 +11,23 @@ angular.module('phantomRunnerApp', [
       .when('/', {
         templateUrl: './views/main.html'
       })
+      .when('/login', {
+        templateUrl: './views/login.html'
+      })
+      .when('/how', {
+        templateUrl: './views/how.html'
+      })
+      .when('/admin', {
+        templateUrl: './views/admin.html'
+      })
       .when('/races', {
         templateUrl: './views/races.html'
       })
       .when('/faq', {
         templateUrl: './views/faq.html'
+      })
+      .when('/contact', {
+        templateUrl: './views/contact.html'
       })
       .otherwise({
         redirectTo: '/'
