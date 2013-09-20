6032cc45d001e4fc5209d5bfd919f7be6b7e5ac4
Lindsay Hertz <lindsay.hertz@gmail.com>
2013-09-20 14:01:09 -0700

    adding views for login / logout / register

--- public/scripts/controllers.js
+++ public/scripts/controllers.js
@@ -25,6 +25,27 @@ angular.module('phantomRunnerApp')
 }]);
 
 angular.module('phantomRunnerApp')
+.controller('RegisterCtrl',
+['$rootScope', '$scope', '$location', 'Auth', function($rootScope, $scope, $location, Auth) {
+    $scope.role = Auth.userRoles.user;
+    $scope.userRoles = Auth.userRoles;
+
+    $scope.register = function() {
+        Auth.register({
+                username: $scope.username,
+                password: $scope.password,
+                role: $scope.role
+            },
+            function() {
+                $location.path('/');
+            },
+            function(err) {
+                $rootScope.error = err;
+            });
+    };
+}]);
+
+angular.module('phantomRunnerApp')
 .controller('LoginCtrl', ['$rootScope', '$scope', '$location', '$window', 'Auth',
   function($rootScope, $scope, $location, $window, Auth){
     $scope.rememberme = true;
