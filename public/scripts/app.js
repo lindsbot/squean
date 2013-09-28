3d164561901536ae67cf628d67b6a5416517c227
Hack Reactor Students <github@hackreactor.com>
2013-09-28 00:15:10 -0700

    Persistent login sorta works

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -14,7 +14,7 @@ angular.module('yourAppHere', ['ngCookies', 'ui.bootstrap'])
       })
       .when('/login', {
         templateUrl: './views/login.html',
-        access: access.public
+        access: access.anon
       })
       .when('/how', {
         templateUrl: './views/how.html',
@@ -36,18 +36,56 @@ angular.module('yourAppHere', ['ngCookies', 'ui.bootstrap'])
         redirectTo: '/',
         access: access.user
       });
+
+      var interceptor = ['$location', '$q', function($location, $q) {
+        function success(response) {
+          return response;
+      }
+
+      function error(response) {
+
+      if(response.status === 401) {
+        $location.path('/login');
+        return $q.reject(response);
+      }
+      else {
+        return $q.reject(response);
+        }
+      }
+
+      return function(promise) {
+        return promise.then(success, error);
+        };
+      }];
+
+      $httpProvider.responseInterceptors.push(interceptor);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   }])
     .run(['$rootScope', '$location', 'Auth', function ($rootScope, $location, Auth) {
 
-        $rootScope.$on("$routeChangeStart", function (event, next, current) {
+        $rootScope.$on('$routeChangeStart', function (event, next, current) {
             $rootScope.error = null;
             if (!Auth.authorize(next.access)) {
-              if (Auth.isLoggedIn(Auth.user)) {
-                $location.path('/');
-              }
-              else {
-                $location.path('/login');
-              }
+              console.log('SCOPE USER ',$rootScope.$root);
+              if (Auth.isLoggedIn($rootScope)) { $location.path('/'); }
+              else { $location.path('/login'); }
 
             }
         });
